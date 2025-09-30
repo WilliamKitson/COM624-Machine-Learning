4d78ccad80e01324b85745247af2c05b3e02e888
4kitsw10 <4kitsw10@solent.ac.uk>
2025-09-30 17:44:20 +0100

    phishing_data_by_type.csv cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,18 +1,17 @@
 import pandas as pd
 
 # Load phishing data to dataframe
-df = pd.read_csv("phishing_data_by_type.csv")
+df = pd.read_csv("CEAS_08.csv")
+df.head()
+
+# View basic structure
+df.info()
 
 # Check for missing values
 df.isnull().sum()
 
-# Convert 'Type' to numeric
-df['Type'] = df['Type'].map({
-    'Fraud': 0,
-    'Commercial Spam': 1,
-    'Phishing': 2,
-    'False Positives ': 3
-})
+# Cleaning data: drop columns not useful for analysis
+df.drop(['receiver', 'date'], axis=1, inplace=True)
 
 # Save cleaned data set
-df.to_csv('phishing_data.csv', index=False)
+df.to_csv('cleaned_training_data.csv', index=False)
