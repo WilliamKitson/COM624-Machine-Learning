19e8512aacc0038634089566bc055969088a061a
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-07 17:54:55 +0100

    feature engineering and EDA.py redefined

--- feature engineering and EDA.py
+++ feature engineering and EDA.py
@@ -15,12 +15,14 @@ print(df.describe(include='all'))
 
 # Calculate 10 most common domains and categorise all others under 'other'
 common = df['sender'].value_counts().nlargest(10).index
-df['Category_top'] = df['sender'].apply(lambda x: x if x in common else 'other')
+df['domains'] = df['sender'].apply(lambda x: x if x in common else 'other')
 
-# Bar chart to show most common domains
-missing_counts = df['Category_top'].value_counts()
-missing_counts = missing_counts[missing_counts.index != 'other']
-missing_counts.plot(kind='bar', color='orange')
-plt.title("Missing Values per Column")
-plt.ylabel("Count")
+# Bar chart to show phishing vs non phishing emails from most common domains
+domain_counts = df.groupby(['domains', 'label']).size().unstack(fill_value=0)
+domain_counts = domain_counts.loc[domain_counts.index != 'other']
+domain_counts.plot(kind='bar', stacked=True, color=['orange', 'blue'])
+plt.title("Phishing vs Non-Phishing Emails by Domain")
+plt.ylabel("Emails")
+plt.xlabel("Domains")
+plt.legend(title="Email Type", labels=['Non-Phishing', 'Phishing'])
 plt.show()
