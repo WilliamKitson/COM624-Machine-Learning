8c020f24d6bb8e1dd2a761c95faf9a6f1a73a409
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-07 17:46:55 +0100

    feature engineering and EDA.py redefined

--- feature engineering and EDA.py
+++ feature engineering and EDA.py
@@ -1,15 +1,26 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 # Load phishing data to dataframe
 df = pd.read_csv("cleaned_training_data.csv")
-print(df.shape)
 
-# EDA: subject length
+# EDA: body length
 df['body_length'] = df["body"].str.len()
 
 # EDA: how many links does the body contain
 df['link_count'] = df['body'].str.count('http')
 
-# Save EDA prepped data set
-df.to_csv('cleaned_training_data_EDA.csv', index=False)
-print(df.shape)
+# Get summary statistics for all columns
+print(df.describe(include='all'))
+
+# Calculate 10 most common domains and categorise all others under 'other'
+common = df['sender'].value_counts().nlargest(10).index
+df['Category_top'] = df['sender'].apply(lambda x: x if x in common else 'other')
+
+# Bar chart to show phishing vs non-phishing
+missing_counts = df['Category_top'].value_counts()
+missing_counts = missing_counts[missing_counts.index != 'other']
+missing_counts.plot(kind='bar', color='orange')
+plt.title("Missing Values per Column")
+plt.ylabel("Count")
+plt.show()
