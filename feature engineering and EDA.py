e8f552e01e3ba6705d384d147b75bca04ab0dafe
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-07 17:58:41 +0100

    feature engineering and EDA.py redefined

--- feature engineering and EDA.py
+++ feature engineering and EDA.py
@@ -10,19 +10,6 @@ df['body_length'] = df["body"].str.len()
 # EDA: how many links does the body contain
 df['link_count'] = df['body'].str.count('http')
 
-# Get summary statistics for all columns
-print(df.describe(include='all'))
-
-# Calculate 10 most common domains and categorise all others under 'other'
-common = df['sender'].value_counts().nlargest(10).index
-df['domains'] = df['sender'].apply(lambda x: x if x in common else 'other')
-
-# Bar chart to show phishing vs non phishing emails from most common domains
-domain_counts = df.groupby(['domains', 'label']).size().unstack(fill_value=0)
-domain_counts = domain_counts.loc[domain_counts.index != 'other']
-domain_counts.plot(kind='bar', stacked=True, color=['orange', 'blue'])
-plt.title("Phishing vs Non-Phishing Emails by Domain")
-plt.ylabel("Emails")
-plt.xlabel("Domains")
-plt.legend(title="Email Type", labels=['Non-Phishing', 'Phishing'])
-plt.show()
+# Save EDA data set for use in Tableau
+df.to_csv('experimental_data_analysis.csv', index=False)
+print(df.shape, df.head())
