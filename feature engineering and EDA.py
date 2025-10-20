d234139a4905d4ca00da2e11d508b42c0669fa52
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-20 17:04:30 +0100

    feature engineering and EDA.py redefined

--- feature engineering and EDA.py
+++ feature engineering and EDA.py
@@ -1,7 +1,12 @@
+import os
 import pandas as pd
 
+# Ensure that cleaned directory exists
+cleaned_dir = 'datasets'
+os.makedirs(cleaned_dir, exist_ok=True)
+
 # Load phishing data to dataframe
-df = pd.read_csv("datasets/cleaned_training_data.csv")
+df = pd.read_csv(os.path.join(cleaned_dir, "cleaned_training_data.csv"))
 
 # EDA: body length
 df['body_length'] = df["body"].str.len()
@@ -10,5 +15,5 @@ df['body_length'] = df["body"].str.len()
 df['link_count'] = df['body'].str.count('http')
 
 # Save EDA data set for use in Tableau
-df.to_csv('datasets/experimental_data_analysis.csv', index=False)
+df.to_csv(os.path.join(cleaned_dir, 'experimental_data_analysis.csv'), index=False)
 print(df.shape, df.head())
