c16537cc7f28198c62bf5d6ae6a1b6a1a51caaee
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-20 14:57:17 +0100

    data collection and preprocessing redefined

--- data collection and preprocessing.py
+++ data collection and preprocessing.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pandas as pd
-from email.utils import parsedate_to_datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load phishing data to dataframe
 df = pd.read_csv("CEAS_08.csv")
 print(df.shape, df.head())
+
+# Visualise columns with missing data before cleaning
+plt.figure(figsize=(10, 5))
+sns.barplot(x=df.isnull().sum().index, y=df.isnull().sum().values, palette='Reds_r')
+plt.title("missing data before cleaning")
+plt.xlabel('column')
+plt.ylabel("missing")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
 
 # Cleaning data: drop columns not useful for analysis
 df.drop(['receiver', 'urls'], axis=1, inplace=True)
@@ -29,6 +40,16 @@ df['subject'] = df['subject'].fillna('')
 
 # Cleaning: drop columns with missing values
 df = df.dropna()
+
+# Visualise columns with missing data after cleaning
+plt.figure(figsize=(10, 5))
+sns.barplot(x=df.isnull().sum().index, y=df.isnull().sum().values, palette='Reds_r')
+plt.title("missing data after cleaning")
+plt.xlabel('column')
+plt.ylabel("Count of Missing Entries")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
 
 # Save cleaned data set
 df.to_csv('cleaned_training_data.csv', index=False)
