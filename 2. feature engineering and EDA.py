51608af95d48a376836306a30a275d7ffd319814
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-24 12:37:40 +0100

    2. feature engineering and EDA redefined

--- 2. feature engineering and EDA.py
+++ 2. feature engineering and EDA.py
@@ -19,15 +19,14 @@ df['body_length'] = df["body"].str.len()
 # EDA: how many links does the body contain
 df['link_count'] = df['body'].str.count('http')
 
-# EDA: most common domains for phishing and safe emails
+# EDA: bar chart most common domains for phishing and safe emails
 email_types = {
     'phishing' : df[df['label'] == 1],
-    'safe': df[df['label'] == 0]
+    'safe' : df[df['label'] == 0]
 }
 
 for name, email_type in email_types.items():
     top_domains_df = email_type['sender'].value_counts().head(10)
-    plt.figure(figsize=(10, 6))
     sns.barplot(x=top_domains_df.values, y=top_domains_df.index, orient='h')
     plt.title('most common domains for ' + name + ' emails')
     plt.xlabel('phishing count')
@@ -35,7 +34,20 @@ for name, email_type in email_types.items():
     plt.tight_layout()
     plt.show()
 
-# EDA:
+# EDA: boxplot body, subject, and link count by phishing and safe emails
+columns = {
+    'body length' : 'body_length',
+    'subject length' : 'subject_length',
+    'link count' : 'link_count'
+}
+
+for name, column in columns.items():
+    df.boxplot(column=column, by='label')
+    plt.title(f'{name} by label')
+    plt.suptitle('')
+    plt.xlabel('Label (0=Safe, 1=Phishing)')
+    plt.ylabel(name)
+    plt.show()
 
 # Save EDA data set
 df.to_csv(os.path.join(cleaned_dir, 'experimental_data_analysis.csv'), index=False)
