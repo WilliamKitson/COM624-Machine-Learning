e615be50f424dc392fdc6dfadc91feb11170dee1
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-26 16:58:55 +0000

    exploratory data analysis.py redefined

--- feature engineering.py
+++ feature engineering.py
@@ -24,7 +24,10 @@ spellchecker = SpellChecker()
 def count_correct_spellings(text):
     return len(spellchecker.known(str(text).split()))
 
-df['correct_spellings_count'] = df['subject'].apply(count_correct_spellings) + df['body'].apply(count_correct_spellings)
+df['correct_spellings_scaled'] = df['subject'].apply(count_correct_spellings) + df['body'].apply(count_correct_spellings)
+
+# Scale correct spellings by body length
+df['correct_spellings_scaled'] = df['correct_spellings_scaled'] / df['body_length'] + df['subject_length']
 
 # Save dataset for exploratory data analysis
 utils.save_dataset(df, 'exploratory_data_analysis.csv')
