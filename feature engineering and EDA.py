6e23c738950d97385d79b559b404fe579e8ade63
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-26 14:49:26 +0000

    feature engineering and EDA.py redefined

--- feature engineering and EDA.py
+++ feature engineering and EDA.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from spellchecker import SpellChecker
 
 # Ensure that cleaned directory exists
 cleaned_dir = 'datasets'
@@ -19,10 +20,18 @@ df['body_length'] = df["body"].str.len()
 # Feature engineering: how many links does the body contain
 df['link_count'] = df['body'].str.count('http')
 
-#Feature engineering: extract and round hour from date
+# Feature engineering: extract and round hour from date
 df['hour'] = pd.to_datetime(df['date'], errors='coerce', utc=True)
 df['hour'] = df['hour'].dt.round('h')
 df['hour'] = df['hour'].dt.hour
+
+# Feature engineering: volume of misspelled words
+spellchecker = SpellChecker()
+
+def count_misspellings(text):
+    return len(spellchecker.unknown(str(text).split()))
+
+df['misspellings_count'] = df['subject'].apply(count_misspellings) + df['body'].apply(count_misspellings)
 
 # EDA: bar chart most common domains for phishing and safe emails
 email_types = {
