ead1d436a693159ac3a1afda897e9773325e936e
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-26 17:43:11 +0000

    traditional models.py redefined

--- traditional models.py
+++ traditional models.py
@@ -1,19 +1,19 @@
 import utils
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
 
+# load exploratory data analysis dataset
 df = utils.load_dataset('exploratory_data_analysis.csv')
 
-# Split dataset into features (x) and target variables (y)
+# split dataset into features (x) and target variables (y)
 x = df[['subject_length', 'body_length', 'link_count', 'hour', 'correct_spellings_scaled']]
 y = df['label']
 
-# Define 80% training and 20% test data
+# define 80% training and 20% test data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(x_train)
@@ -26,19 +26,11 @@ models = {
 'Naive Bayes': GaussianNB(),
 'XGBoost': XGBClassifier()
 }
-results = {
-}
-predictions = {
-}
 
 #
 for name, model in models.items():
     model.fit(x_train_scaled, y_train)
     y_pred = model.predict(x_test_scaled)
-    predictions[name] = y_pred
-    print(f"\n{name} Classification Report:\n")
-    print(classification_report(y_test, y_pred, zero_division=0))
-    metrics = utils.evaluate_model(y_test, y_pred)
-    results[name] = metrics
-    utils.plot_metrics(name, metrics)
     utils.save_model(name, model)
+    utils.visualise_model(name, y_test, y_pred)
+    print(utils.model_performance(y_test, y_pred))
--- utils.py
+++ utils.py
@@ -34,10 +34,11 @@ def visualise_missing_rows(df):
     plt.tight_layout()
     plt.show()
 
-def evaluate_model(y_true_in, y_pred_in):
+def visualise_model(name, y_true_in, y_pred_in):
     y_true_oh = pd.get_dummies(y_true_in)
     y_pred_oh = pd.get_dummies(y_pred_in)
-    return {
+
+    evaluation = {
         'Accuracy': accuracy_score(y_true_in, y_pred_in),
         'Precision': precision_score(y_true_in, y_pred_in, average='weighted', zero_division=0),
         'Recall': recall_score(y_true_in, y_pred_in, average='weighted', zero_division=0),
@@ -45,12 +46,22 @@ def evaluate_model(y_true_in, y_pred_in):
         'ROC-AUC': roc_auc_score(y_true_oh, y_pred_oh, multi_class='ovr')
     }
 
-def plot_metrics(name_in, metrics_in):
     plt.figure(figsize=(6, 4))
-    sns.barplot(x=list(metrics_in.keys()), y=list(metrics_in.values()))
-    plt.title(f"{name_in} Performance Metrics")
+    sns.barplot(x=list(evaluation.keys()), y=list(evaluation.values()))
+    plt.title(f'{name} Performance Metrics')
     plt.ylabel("Score")
     plt.ylim(0, 1)
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
+
+def model_performance(y_true_in, y_pred_in):
+    metrics_results = {
+        accuracy_score(y_true_in, y_pred_in),
+        precision_score(y_true_in, y_pred_in, average='weighted', zero_division=0),
+        recall_score(y_true_in, y_pred_in, average='weighted', zero_division=0),
+        f1_score(y_true_in, y_pred_in, average='weighted', zero_division=0),
+        roc_auc_score(y_true_in, y_pred_in, multi_class='ovr')
+    }
+
+    return sum(metrics_results) / len(metrics_results)
