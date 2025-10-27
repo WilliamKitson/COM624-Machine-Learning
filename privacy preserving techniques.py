581c175f58feea0046f70397232fdee23884cf32
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-27 12:49:54 +0000

    privacy preserving techniques.py defined

---
+++ privacy preserving techniques.py
@@ -0,0 +1,23 @@
+import utils
+import re
+
+# load exploratory data analysis dataset
+df = utils.load_dataset('exploratory_data_analysis.csv')
+
+text_columns = {
+    'subject',
+    'body'
+}
+
+# anonymise links in subject and body
+for column in text_columns:
+    df[f'{column}_anonymised'] = df[column].astype(str).apply(lambda x: re.sub(r'http\S+', '[LINK]', x))
+
+# anonymise emails within subject and body
+
+# anonymise numbers within subject and body
+
+# anonymise dates within subject and body
+
+# save dataset for exploratory data analysis
+utils.save_dataset(df, 'privacy_preserved.csv')
