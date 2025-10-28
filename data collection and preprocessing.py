60cf278b6e893013fb3c051b4c62e47a93491435
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-28 17:47:55 +0000

    feature engineering.py redefined

--- data collection and preprocessing.py
+++ data collection and preprocessing.py
@@ -2,16 +2,21 @@ import utils
 
 # load dataset and visualise missing rows
 df = utils.load_dataset('CEAS_08.csv')
-utils.visualise_missing_rows(df)
 
 # drop columns not useful for analysis
 df.drop(['urls'], axis=1, inplace=True)
 
-# replace null subjects with empty string
-df['subject'] = df['subject'].fillna('')
+# replace null subjects and receivers with empty string
+utils.visualise_missing_rows(df)
 
-# drop columns with missing values and visualise
-df = df.dropna()
+columns = {
+    'subject',
+    'receiver'
+}
+
+for column in columns:
+    df[column] = df[column].fillna('')
+
 utils.visualise_missing_rows(df)
 
 # Save cleaned data set
