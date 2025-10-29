41051a3b8f628cc80c4064b5640471766522d308
4kitsw10 <4kitsw10@solent.ac.uk>
2025-10-29 14:13:29 +0000

    data collectiojn and preprocessing redefined

--- data collection and preprocessing.py
+++ data collection and preprocessing.py
@@ -1,7 +1,21 @@
 import utils
+import pandas as pd
 
 # load dataset and visualise missing rows
 df = utils.load_dataset('CEAS_08.csv')
+
+# convert object type columns to more useful datatypes
+columns_string_type = {
+    'sender',
+    'receiver',
+    'subject',
+    'body'
+}
+
+for column in columns_string_type:
+    df[column] = df[column].astype('string')
+
+df['date'] = pd.to_datetime(df['date'], errors='coerce', utc=True)
 
 # drop columns not useful for analysis
 df.drop(['urls'], axis=1, inplace=True)
