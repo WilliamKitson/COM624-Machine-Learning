048ec562daf62acf6da246f0629f7afb4bb0dbc3
4kitsw10 <4kitsw10@solent.ac.uk>
2025-11-03 16:24:25 +0000

    learning pca.py defined

--- learning pca.py
+++ learning pca.py
@@ -1,4 +1,5 @@
 import utils
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
