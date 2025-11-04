635016ac5b6dcdb1dce28048065bab070b272904
4kitsw10 <4kitsw10@solent.ac.uk>
2025-11-04 16:27:50 +0000

    clustering and grouping.py redefined

--- clustering and grouping.py
+++ clustering and grouping.py
@@ -1,11 +1,43 @@
 import utils
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # load dataset
 df = utils.load_dataset('feature_engineered_dataset.csv')
+
+#
+x = df.iloc[:, [8, 9]].values
+
+#
+plt.scatter(x[:, 0], x[:, 1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('temp')
+plt.show()
+
+#
+within_cluster_sum_of_squares = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(x)
+    within_cluster_sum_of_squares.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), within_cluster_sum_of_squares)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('within cluster sum of squares')
+plt.show()
+
+
+
+
+
+
 
 # aggregate all text columns
 text_columns = [
