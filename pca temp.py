f5133dc33517f5d2e232bbffb5e5a0fd628daeda
4kitsw10 <4kitsw10@solent.ac.uk>
2025-11-21 09:28:48 +0000

    pca temp redefined

--- pca temp.py
+++ pca temp.py
@@ -5,6 +5,7 @@ from sklearn.decomposition import PCA
 from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
 
 # load dataset
 df = utils.load_dataset('feature_engineered_dataset.csv')
@@ -39,11 +40,46 @@ df_pca = pd.DataFrame(x_pca, columns=[
     'principal_component_2'
 ])
 
-# visualise principal components
-plt.scatter(df_pca['principal_component_1'], df_pca['principal_component_2'], alpha=0.7)
-plt.xlabel('principal component 1')
-plt.ylabel('principal component 2')
-plt.title('PCA of aggregated text')
+#
+x = df_pca.iloc[:, [0, 1]].values
+
+#
+plt.scatter(x[:, 0], x[:, 1])
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.title('PCA')
+plt.show()
+
+#
+inertia = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(x)
+    inertia.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), inertia)
+plt.title('elbow method')
+plt.xlabel('clusters count')
+plt.ylabel('inertia')
+plt.show()
+
+# Training the K-Means model on the dataset
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(x)
+
+# Visualising the clusters
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s=100, c='magenta', label='Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+plt.title('temp')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
 plt.show()
 
 # save training dataset
