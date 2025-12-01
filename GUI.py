a9c2ac35f9039271d31a16f58968aef2096abe49
4kitsw10 <4kitsw10@solent.ac.uk>
2025-12-01 09:57:32 +0000

    GUI redefined

--- GUI.py
+++ GUI.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="4kitsw10 COM624 AE1", layout="wide")
 st.title("Phishing Email Detection")
 
 st.markdown(
-    "This is the GUI for my COM624 Machine Learning project."
+    "Please use the below tools to run a report on simulated emails."
 )
 
 st.selectbox("Select Model", [
@@ -16,6 +16,7 @@ st.selectbox("Select Model", [
     "BERT"
 ])
 st.text_input(label="Sender", placeholder="some.dude@someplace.com")
+st.time_input(label="Time")
 st.text_input(label="Subject", placeholder="Some Subject")
 st.text_area(label="Body",placeholder="Some Body")
 st.button(label="Submit", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
